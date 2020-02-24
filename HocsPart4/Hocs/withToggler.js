8f715976bb04e5f89805ca4282836d09b8a97767
lovebhatia <javalove007@gmail.com>
2020-02-24 14:35:41 +0530

    1 file modified

--- HocsPart4/Hocs/withToggler.js
+++ HocsPart4/Hocs/withToggler.js
@@ -1,13 +1,12 @@
-import React from  'react';
-export function withToggler(component)
-{
-  return function(props)
-  {
-    return (
-      <Toggler component={component} {...props}/>
-    )
+import React from "react"
+// HOC is a function that takes a component as a parameter
+// and returns a new component wrapping the given component
+// and "supercharging" it by giving it some extra abilities
 
-  }
-
+export function withToggler(component) {
+    return function(props) {
+        return (
+            <Toggler component={component} {...props}/>
+        )
+    }
 }
- 
