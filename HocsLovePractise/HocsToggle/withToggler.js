10cfd4024f09d9886c6628243ca6cbd8480d78a8
lovebhatia <javalove007@gmail.com>
2020-02-24 17:10:07 +0530

    1 file added

---
+++ HocsLovePractise/HocsToggle/withToggler.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//Hoc is a function that takes a component as a parameter and returns the new component wrapping the given component and supercharging it by giving it some extra capabilities
+function withToggler()
+{
+
+}
