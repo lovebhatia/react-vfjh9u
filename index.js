2e24813876e83e08b4c2a1f987580179b96b3cdd
lovebhatia <javalove007@gmail.com>
2020-03-09 16:06:56 +0530

    5 files modified

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import AppRenderProps2 from './RenderProps2/AppRenderProps2'
 import AppHocsLove from './HocsLovePractise/AppHocsLove'
 import AppCompUpdate from './ShoulComponentUpdate/AppCompUpdate'
 import AppReactMemo from './ReactMemo/AppReactMemo'
+import AppContextProvider from './ReactContext/ContextProvider/AppContextProvider';
 
 //render(<AppHocsPart someprophere="hi there"></AppHocsPart>, document.getElementById('root'));
-render(<AppReactMemo></AppReactMemo>, document.getElementById('root'));
+render(<AppContextProvider></AppContextProvider>, document.getElementById('root'));
