7b1db6e46b6a5df64ee7215d7528813b0ec4f3fb
lovebhatia <javalove007@gmail.com>
2020-03-10 23:25:29 +0530

    4 files added

---
+++ ReactContext/ContextConsumer/AppContextConsumer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Header from "./Header"
+import Button from "./Button"
+
+function AppContextConsumer() {
+    return (
+        <div>
+            <Header />
+            <Button />
+        </div>
+    )
+}
+
+export default AppContextConsumer
