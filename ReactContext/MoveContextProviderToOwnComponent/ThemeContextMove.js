488fc9cd651447f1382a78d808a9f1f3ab3fee81
lovebhatia <javalove007@gmail.com>
2020-03-17 00:07:17 +0530

    1 file modified

--- ReactContext/MoveContextProviderToOwnComponent/ThemeContextMove.js
+++ ReactContext/MoveContextProviderToOwnComponent/ThemeContextMove.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React,{Component} from "react"
 const ThemeContextMove = React.createContext()
+class ThemeContextProvider extends component{
+  render()
+  {
+    return(
+
+    )
+  }
+}
+
 export default ThemeContextMove
