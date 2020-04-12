fa85762a3f0378a05c94d53a306e95a1512c13b4
lovebhatia <javalove007@gmail.com>
2020-04-12 20:36:06 +0530

    3 files modified

--- ReactRouter/SwitchRoute/AppSwitchRoute.js
+++ ReactRouter/SwitchRoute/AppSwitchRoute.js
@@ -1,5 +1,8 @@
 import React from "react"
 import {Link, Switch, Route} from "react-router-dom"
+import Home from './Home';
+import About from './About';
+
 
  function AppSwitchRoute()
  {
@@ -8,10 +11,14 @@ import {Link, Switch, Route} from "react-router-dom"
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Switch>
-            <Route exact path="/" render={() => <h1>Home Page!</h1>}/>
-            <Route path="/about" render={() => <h1>About Page!</h1>}/>
-            </Switch>
 
+           // <Route exact path="/" render={() => <Home/>}/>
+           // <Route  path="/about" render={() => <About/>}/>
+
+           <Route exact path="/" component={Home}/>
+            <Route  path="/about" render={() => <About/>}/>
+            </Switch>
+ 
 
         </div>
    )
