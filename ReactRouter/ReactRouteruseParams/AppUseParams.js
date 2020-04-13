87643c56694fb1e764b8cc9115011d2fd1a6cbd4
love bhatia <javalove007@gmail.com>
2020-04-13 20:11:52 +0530

    Create AppUseParams.js

---
+++ ReactRouter/ReactRouteruseParams/AppUseParams.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import ServicesList from "./pages/services/ServicesList"
+import ServiceDetail from "./pages/services/ServiceDetail"
+
+import {Switch, Route} from "react-router-dom"
+
+function AppUseParam() {    
+    return (
+        <div>
+            <Header />
+            
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/services" >
+                    <ServicesList />
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default AppUseParam
