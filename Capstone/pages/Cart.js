4033f37d12613e316d111ef093728fcb1a4c5bb2
lovebhatia <javalove007@gmail.com>
2020-07-05 23:46:46 +0530

    2 files modified

---
+++ Capstone/pages/Cart.js
@@ -0,0 +1,8 @@
+import React from "react"
+function Cart(){
+  return(
+    <main className="cart-page">
+      <h1>Check out</h1>
+      </main>
+  )
+}
---
+++ Capstone/pages/Photos.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Photos(){
+  return(
+    <main className="photos">
+      <h1>Images goes here</h1>
+      </main>
+  )
+}
